dac2ed309a8edf643d43da4423c3890dfbcbbda2
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-08-14 18:12:30 +0000

    Added list of variables for quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,23 @@
+// Variables
+
+let question = document.getElementById("question");
+let container = document.getElementById("container");
+let scorecard = document.getElementById('scorecard');
+let option0 = document.getElementById('option0');
+let option1 = document.getElementById('option1');
+let option2 = document.getElementById('option2');
+let option3 = document.getElementById('option3');
+let next = document.querySelector('.next');
+let points = document.getElementById('score');
+let span = document.querySelectorAll('span');
+let i = 0;
+let score = 0;
+
+
+
 // Quiz questions
 
-let answersForQuiz = [
-    {
+let answersForQuiz = [{
         question: 'What is the word Halloween an abbreviation of?',
         option: ["All Hallow's Eve", "All Hallows Day", "All Saints Day", "On All Saints' Eve"],
         answer: "All Hallow's Eve"
