2a2b6b20a8d6272102bc5d160c488741d0b4e75f
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-08-14 17:54:36 +0000

    Created structure for question in array

---
+++ assets/js/quiz.js
@@ -0,0 +1,55 @@
+// Quiz questions
+
+let answersForQuiz = [
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    {
+        question: '',
+        option: [''],
+        answer: ''
+    },
+    
+]
